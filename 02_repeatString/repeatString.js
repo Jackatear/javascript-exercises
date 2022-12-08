d9584223d583f20adef89b4999af9f4f5088d8f8
Jackatear <jackatear@gmail.com>
2022-12-08 11:13:21 +0000

    Odin repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(x, y) {
+    if (y < 0) {
+        return "ERROR";
+    }
+    return x.repeat(y)
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
+ 
