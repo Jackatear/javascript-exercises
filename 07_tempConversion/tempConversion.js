e74f9976e1e5f68c13da31fe9ef3b8ea556ed990
Jackatear <jackatear@gmail.com>
2022-12-08 17:58:11 +0000

    Odin tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  if (typeof(n) != 'number'){
+    return "ERROR";
+  }
+
+  let result = (n -32) * (5/9);
+
+  let roundedResult = Math.round(result * 10) / 10;
+
+  return roundedResult;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  if (typeof(n) != 'number'){
+    return "ERROR";
+  }
+
+  let result = (n * (9/5)) + 32;
+
+  let roundedResult = Math.round(result * 10) / 10;
+
+  return roundedResult;
 };
+
 
 // Do not edit below this line
 module.exports = {
