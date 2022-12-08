275ea414e864b677b76141d78177318e60808a5c
Jackatear <jackatear@gmail.com>
2022-12-08 11:30:34 +0000

    Odin reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var strArray = string.split("");
+    var reverseArray = strArray.reverse();
+    var reverseString = reverseArray.join("");
+
+    return reverseString;
 
 };
+
+console.log(reverseString("My name is Jack"))
 
 // Do not edit below this line
 module.exports = reverseString;
